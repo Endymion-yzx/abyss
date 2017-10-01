ba8b569099446328e320a5cf404993e8d76f9ffd
Endymion <495609715@qq.com>
2017-10-01 14:21:06 +0800

    Modified run method

--- abyss_session.py
+++ abyss_session.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from random import randint
 import getpass
 import subprocess
+import re
 
 class AbyssSession():
 	def __init__(self):
@@ -18,78 +19,85 @@ class AbyssSession():
 		self._worker_hosts = []
 		self._ps_hosts = []
 		for i in range(self._worker_replicas):
-		    #worker_name = self._container_name + '-worker-' + str(i) + ':' + str(self._port)
-		    worker_addr = '10.100.100.' + str(i) + ':' + str(self._worker_port)
-		    self._worker_hosts.append(worker_addr)
+			#worker_name = self._container_name + '-worker-' + str(i) + ':' + str(self._port)
+			worker_addr = '10.100.100.' + str(i) + ':' + str(self._worker_port)
+			self._worker_hosts.append(worker_addr)
 
 		for j in range(self._ps_replicas):
-		    #ps_name = self._container_name + '-ps-' + str(j) + ':' + str(self._port)
-		    ps_addr = '10.100.101.' + str(j) + ':' + str(self._worker_port)
-		    self._ps_hosts.append(ps_addr)
+			#ps_name = self._container_name + '-ps-' + str(j) + ':' + str(self._port)
+			ps_addr = '10.100.101.' + str(j) + ':' + str(self._worker_port)
+			self._ps_hosts.append(ps_addr)
 		# TODO: dynamically allocate IP or use DNS
 		#self._namespace = 'tensorflow'
 		self._namespace = 'default'
 		self._image = 'ywanggf/multi_container:v2'
 		self._script = '/container.py'
+
+		self._job_map = {}      # Match machines required by user with containers
+		for job in ['worker', 'ps']:
+			ctn_job = 'container-' + job
+			self._job_map[job] = ctn_job
+		self._cluster_spec = {
+			self._job_map['worker']: self._worker_hosts, 
+			self._job_map['ps']: self._ps_hosts, 
+			# TODO: decide the coordinator's address
+			'coord': ['192.168.2.110:'+str(self._coord_port)]}
 		
 		for job in ['worker', 'ps']:
-		    for i in range(self._replicas[job]):
 
-			# Start a container
-			config.load_kube_config()
-			service = client.V1Service()
-			service.api_version = 'v1'
-			service.kind = 'Service'
-			service.metadata = client.V1ObjectMeta(name=(self._container_name + '-' + job + '-' + str(i)))
-			srvSpec = client.V1ServiceSpec()
-			srvSpec.selector = { 'name': self._container_name + '-' + job + '-' + str(i), 'job': job, 'task': str(i) }
-			srvSpec.ports = [client.V1ServicePort(port=self._worker_port)]
-			if job == 'worker':
-			    srvSpec.cluster_ip = self._worker_hosts[i].split(':')[0]
-			if job == 'ps':
-			    srvSpec.cluster_ip = self._ps_hosts[i].split(':')[0]
-			service.spec = srvSpec
+			for i in range(self._replicas[job]):
+				# Start a container
+				config.load_kube_config()
+				service = client.V1Service()
+				service.api_version = 'v1'
+				service.kind = 'Service'
+				service.metadata = client.V1ObjectMeta(name=(self._container_name + '-' + job + '-' + str(i)))
+				srvSpec = client.V1ServiceSpec()
+				srvSpec.selector = { 'name': self._container_name + '-' + job + '-' + str(i), 'job': job, 'task': str(i) }
+				srvSpec.ports = [client.V1ServicePort(port=self._worker_port)]
+				if job == 'worker':
+					srvSpec.cluster_ip = self._worker_hosts[i].split(':')[0]
+				if job == 'ps':
+					srvSpec.cluster_ip = self._ps_hosts[i].split(':')[0]
+				service.spec = srvSpec
 
-			api_instance = client.CoreV1Api()
-			api_instance.create_namespaced_service(namespace=self._namespace, body=service)
-			self._resources['service'].append(service)
+				api_instance = client.CoreV1Api()
+				api_instance.create_namespaced_service(namespace=self._namespace, body=service)
+				self._resources['service'].append(service)
 
-			replicaset = client.V1beta1ReplicaSet()
-			replicaset.api_version = 'extensions/v1beta1'
-			replicaset.kind = 'ReplicaSet'
-			replicaset.metadata = client.V1ObjectMeta(name=(self._container_name + '-' + job + '-' + str(i)))
-			rsSpec = client.V1beta1ReplicaSetSpec()
-			rsSpec.replicas = 1
+				replicaset = client.V1beta1ReplicaSet()
+				replicaset.api_version = 'extensions/v1beta1'
+				replicaset.kind = 'ReplicaSet'
+				replicaset.metadata = client.V1ObjectMeta(name=(self._container_name + '-' + job + '-' + str(i)))
+				rsSpec = client.V1beta1ReplicaSetSpec()
+				rsSpec.replicas = 1
 
-			template = client.V1PodTemplateSpec()
-			template.metadata = client.V1ObjectMeta(labels={'name':self._container_name + '-' + job + '-' + str(i), 'job':job, 'task':str(i)})
-			podSpec = client.V1PodSpec()
-			container = client.V1Container()
-			container.name = 'tensorflow'
-			container.image = self._image
-			container.ports = [client.V1ContainerPort(self._worker_port)]
-			container.command = ['/usr/bin/python', self._script]
-			container.args = []
-			# Command line arguments
-			container.args.append('--job_name=' + job)
-			container.args.append('--task_index=' + str(i))
+				template = client.V1PodTemplateSpec()
+				template.metadata = client.V1ObjectMeta(labels={'name':self._container_name + '-' + job + '-' + str(i), 'job':job, 'task':str(i)})
+				podSpec = client.V1PodSpec()
+				container = client.V1Container()
+				container.name = 'tensorflow'
+				container.image = self._image
+				container.ports = [client.V1ContainerPort(self._worker_port)]
+				container.command = ['/usr/bin/python', self._script]
+				container.args = []
+				# Command line arguments
+				container.args.append('--job_name=' + self._job_map[job])
+				container.args.append('--task_index=' + str(i))
 
-			container.args.append('--ps_hosts='+','.join(map(str,self._ps_hosts)))
-			container.args.append('--worker_hosts='+','.join(map(str,self._worker_hosts)))
-			container.args.append('--coord_host=192.168.2.110:'+str(self._coord_port))
-			print 'args:'
-			print container.args
-			podSpec.containers = [container]
-			template.spec = podSpec
-			rsSpec.template = template
-			replicaset.spec = rsSpec
-			api_instance_rs = client.ExtensionsV1beta1Api()
-			api_instance_rs.create_namespaced_replica_set(namespace=self._namespace, body=replicaset)
-			self._resources['replicaset'].append(replicaset)
+				container.args.append('--cluster_spec=' + str(self._cluster_spec))
+				print 'args:'
+				print container.args
+				podSpec.containers = [container]
+				template.spec = podSpec
+				rsSpec.template = template
+				replicaset.spec = rsSpec
+				api_instance_rs = client.ExtensionsV1beta1Api()
+				api_instance_rs.create_namespaced_replica_set(namespace=self._namespace, body=replicaset)
+				self._resources['replicaset'].append(replicaset)
 
 		# Create the cluster
-		# TODO: decide the coordinator's address
-		cluster = tf.train.ClusterSpec({'worker': self._worker_hosts, 'ps': self._ps_hosts, 'coord': ['192.168.2.110:'+str(self._coord_port)]})
+		cluster = tf.train.ClusterSpec(self._cluster_spec)
 		print 'cluster:'
 		print(cluster.__dict__)
 		server = tf.train.Server(cluster, job_name='coord', task_index=0)
@@ -98,18 +106,12 @@ class AbyssSession():
 		self._sess = tf.Session(server.target)
 
 	def run(self, fetches, feed_dict=None, options=None, run_metadata=None):
-		# if isinstance(fetches, tf.Tensor):
-		# 	g = fetches.graph
-		# 	nodes = g._nodes_by_id.values()
-		# 	for node in nodes:
-		# 		node._set_device('/job:worker/task:0')
-		# else:
-		# 	fetch_list = list(fetches)
-		# 	for fetch in fetch_list:
-		# 		g = fetch.graph
-		# 		nodes = g._nodes_by_id.values()
-		# 		for node in nodes:
-		# 			node._set_device('/job:worker/task:0')
+		graph = self._sess.graph
+		nodes = graph._nodes_by_id.values()
+		for node in nodes:
+			device = node.device        # What if device is empty?
+			if re.search(r'job:([^\/]*)', device):
+				node._set_device(re.sub(r'job:([^\/]*)', lambda match: self._job_map[match.group(1)], device))
 
 		return self._sess.run(fetches, feed_dict, options, run_metadata)
 
--- container.py
+++ container.py
@@ -1,20 +1,14 @@
 import tensorflow as tf
 
 flags = tf.app.flags
-flags.DEFINE_string("ps_hosts","localhost:2222",
-                    "One hostname:port pair")
-flags.DEFINE_string("worker_hosts", "localhost:2223",
-                    "Comma-separated list of hostname:port pairs")
-flags.DEFINE_string("coord_host","localhost:2222",
-                    "One hostname:port pair")
-flags.DEFINE_string("job_name", None,"job name: ps or worker")
+
+flags.DEFINE_string("job_name", None, "job name in the cluster")
 flags.DEFINE_integer("task_index", None,
                      "Worker task index, should be >= 0.")
+flags.DEFINE_string("cluster_spec", None, "A dictionary mapping jobs with hosts")
 FLAGS = flags.FLAGS
 
-cluster = tf.train.ClusterSpec({'ps': FLAGS.ps_hosts.split(','), 
-			'worker': FLAGS.worker_hosts.split(','),
-			'coord': [FLAGS.coord_host]})
+cluster = tf.train.ClusterSpec(eval(FLAGS.cluster_spec))
 server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
 
 server.join()
